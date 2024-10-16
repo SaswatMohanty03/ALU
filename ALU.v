e5da2980542a75feabd99a2f47249f965af3ca45
SaswatMohanty03 <saswatmohanty741@gmail.com>
2024-10-16 15:08:12 +0530

    Update ALU.v

--- ALU.v
+++ ALU.v
@@ -1,54 +1,35 @@
 `timescale 1ns / 1ps
 
-module alu(input[7:0]A,B,                          
-           input[3:0]ALU_Sel,                    
-           output[7:0]ALU_Out,                   
-           output CarryOut                      
-    );
-    reg [7:0] ALU_Result;
-    wire [8:0]tmp;
-    assign ALU_Out = ALU_Result; 
-    assign tmp = {1'b0,A + 1'b0,B};
-    assign CarryOut = tmp[8];
-    always @(*)
-    begin
-    case (ALU_Sel)
-    4'b0000 :
-            ALU_Result = A + B;               
-    4'b0001 :
-            ALU_Result = A - B;              
-    4'b0010 :
-            ALU_Result = A * B;              
-    4'b0011 :
-            ALU_Result = A / B;              
-    4'b0100 :
-            ALU_Result = A << 1;             
-    4'b0101 :
-            ALU_Result = A >> 1;            
-    4'b0110 :
-            ALU_Result = {A[6:0],A[7]};      
-    4'b0111 :
-            ALU_Result = {A[0],A[7:1]};      
-    4'b1000 :
-            ALU_Result = A & B;              
-    4'b1001 :
-            ALU_Result = A | B;             
-    4'b1010 :
-            ALU_Result = A ^ B;              
-    4'b1011 :
-            ALU_Result = ~(A | B);          
-    4'b1100 :
-            ALU_Result = ~(A & B);           
-    4'b1101 :
-            ALU_Result = ~(A ^ B);           
-    4'b1110 :
-            ALU_Result = (A>B)?8'd1:8'd0;    
-    4'b1111 :
-            ALU_Result = (A==B)?8'd1:8'd0;   
-            
-    default:
-           ALU_Result = A + B;        
-    endcase
-end  
-    
-endmodule
+module ALU(input[7:0]A,
+           input[7:0]B,
+           input[2:0]alu_sel,
+           output reg[7:0]alu_out,
+           output reg carry_out);
+           
+           reg[8:0]temp;
+           
+           always @(*)begin
+           carry_out = 0;
+           case(alu_sel)
+               3'b000: alu_out = A | B;
+               3'b001: alu_out = A & B;
+               3'b010: begin
+                 temp = A+B;
+                 alu_out = temp[7:0];
+                 carry_out = temp[8];
+                 end
+               3'b011: begin
+                 temp = A - B;
+                 alu_out = temp[7:0];
+                 carry_out = temp[8];
+                 end
+               3'b100: alu_out = A ^ B;
+               3'b101: alu_out = ~(A ^ B);
+               3'b110: alu_out = ~(A & B);
+               3'b111: alu_out = ~(A | B);
+               default: alu_out = 8'b00000000;
+               
+           endcase
+               end
+                 
+         endmodule
